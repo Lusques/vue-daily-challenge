46afcebd2e20357183334101494576c89797d75c
Lucas Silva <lucascarlosjobs@gmail.com>
2025-04-09 23:14:09 -0300

    install vue3

--- src/components/challenges/day-01/Index.spec.ts
+++
@@ -1,8 +0,0 @@
-
-import { mount } from '@vue/test-utils'
-import Index from './Index.vue'
-
-test('renderiza corretamente', () => {
-  const wrapper = mount(Index)
-  expect(wrapper.text()).toContain('Desafio Day 1')
-})
